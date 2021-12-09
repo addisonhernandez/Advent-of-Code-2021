5a51b85a70c11c84cfe334988d98bbee368cfd4c
addisonhernandez <addison.hernandez@gmail.com>
2021-12-09 15:08:03 -0500

    Add functionality: parse input and compute part 1

--- day 8/day8.py
+++ day 8/day8.py
@@ -2,9 +2,27 @@
 https://adventofcode.com/2021/day/8
 """
 
+
 def main():
-    pass
+    # Parse input file
+    inputs = []
+    outputs = []
+    with open("day 8/day8test.txt") as infile:
+        for line in infile:
+            ins, _, outs = line.partition(" | ")
+            inputs.append(ins.split())
+            outputs.append(outs.split())
+
+    # Part 1: Count number of 1, 4, 7, 8 in the outputs
+    print(f"Part 1: {one_four_seven_eight(outputs)}")
 
 
-if __name__ == '__main__':
+def one_four_seven_eight(lst: list) -> int:
+    """Counts number of 1, 4, 7, and 8 in a list of 7-segment display signals."""
+
+    unique_lengths = {2, 4, 3, 7}  # one, four, seven, eight
+    return sum(1 for line in lst for item in line if len(item) in unique_lengths)
+
+
+if __name__ == "__main__":
     main()
