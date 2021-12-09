9a87288970d7c5b6222875fa74e1cb09e58a1495
addisonhernandez <addison.hernandez@gmail.com>
2021-12-09 10:20:07 -0500

    Refactor and reformat for clarity.

--- day 2/day2.py
+++ day 2/day2.py
@@ -1,15 +1,30 @@
-'''Advent of Code 2021: Day 2
+"""Advent of Code 2021: Day 2
 https://adventofcode.com/2021/day/2
-'''
+"""
+
+
+def main():
+    # Parse directions from file. 
+    # Directions are of the format '<str: order> <int: magnitude>'
+    with open("day 2/day2.txt", "r") as infile:
+        directions = [line.split() for line in infile]
+
+    # Part 1
+    distance, depth = navigate(directions)
+    print(f"Distance: {distance}\tDepth: {depth}\tProduct: {distance * depth}")
+
+    # Part 2
+    distance, depth = navigate_better(directions)
+    print(f"Distance: {distance}\tDepth: {depth}\tProduct: {distance * depth}")
 
 
 def navigate(directions):
-    '''Parse `directions` and generate a (distance, depth) position tuple.
+    """Parse `directions` and generate a (distance, depth) position tuple.
 
     forward `x`: moves a horizontal distance `x`
     down `x`: increases depth by `x`
     up `x`: decreases depth by `x`
-    '''
+    """
     distance = depth = 0
 
     for direction in directions:
@@ -26,16 +41,16 @@ def navigate(directions):
 
 
 def navigate_better(directions):
-    '''Parse `directions` and generate a (distance, depth) position tuple.
+    """Parse `directions` and generate a (distance, depth) position tuple.
 
     down `x`: increases aim by `x`
     up `x`: decreases aim by `x`
     forward `x`: moves horizontal distance `x` and increases depth by aim * `x`
-    '''
+    """
     distance = depth = aim = 0
 
-    for d in directions:
-        order, magnitude = d[0], int(d[1])
+    for direction in directions:
+        order, magnitude = direction[0], int(direction[1])
 
         if order == "forward":
             distance += magnitude
@@ -49,8 +64,5 @@ def navigate_better(directions):
 
 
 if __name__ == "__main__":
-    with open("day2.txt", "r") as infile:
-        directions = [line.split() for line in infile]
-
-    dis, dep = navigate_better(directions)
-    print(f"Distance: {dis}\tDepth: {dep}\tProduct: {dis * dep}")
+    main()
+    
