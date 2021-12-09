f482ee82eb86c573b14fafc4857dfc5993327eda
addisonhernandez <addison.hernandez@gmail.com>
2021-12-09 10:10:05 -0500

    Refactor for clarity and better docstrings.

--- day 1/day1.py
+++ day 1/day1.py
@@ -1,25 +1,30 @@
-'''Advent of Code 2021
+"""Advent of Code 2021
 https://adventofcode.com/2021/day/1
-'''
+"""
 
 
-def number_of_depth_drops(depths):
-    '''
-    drops = 0
-    for i, depth in enumerate(depths[1:]):
-        if depth > depths[i]:
-            drops += 1
-    '''
+def main():
+    # Read sonar data from file
+    with open("day 1/day1.txt", "r") as infile:
+        depths = [int(line) for line in infile]
+
+    # Part 1
+    print(f"Drops: {depth_drops(depths)}")
+
+    # Part 2
+    print(f"Fuzzy: {fuzzy_drops(depths)}")
+
+
+def depth_drops(depths):
+    """Calculates number of times depth reading increases between two readings."""
     return sum(1 for i, d in enumerate(depths[1:]) if d > depths[i])
 
 
-def number_of_fuzzy_drops(depths):
-    fuzzy_depths = [sum(depths[i:i + 3]) for i in range(len(depths) - 2)]
-    return number_of_depth_drops(fuzzy_depths)
+def fuzzy_drops(depths):
+    """Calculates depth reading increases between averaged readings."""
+    fuzzy_depths = [sum(depths[i : i + 3]) for i in range(len(depths) - 2)]
+    return depth_drops(fuzzy_depths)
 
 
 if __name__ == "__main__":
-    with open("day1.txt", "r") as infile:
-        depths = [int(line) for line in infile]
-    print(f"Drops: {number_of_depth_drops(depths)}")
-    print(f"Fuzzy: {number_of_fuzzy_drops(depths)}")
+    main()
