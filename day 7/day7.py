083b1225ac68fc5fd44c24aa9c29bcc2da19ff45
addisonhernandez <addison.hernandez@gmail.com>
2021-12-09 09:52:08 -0500

    Remove debugging line and clarify cost calculation

--- day 7/day7.py
+++ day 7/day7.py
@@ -6,20 +6,30 @@ import numpy as np
 
 
 def main():
-    crabs = np.ndarray((1,))
     # Parse crab positions from file
     with open("day7.txt") as infile:
         crabs = np.loadtxt(infile, dtype=int, delimiter=",")
 
-    # Calculate the optimal position
+    # Part 1: Calculate optimal position for constant fuel usage
     pos = int(np.median(crabs))
     print(f"{pos = }\tTotal cost: {total_cost(crabs, pos)}")
 
+    # Part 2: Calculate optimal position for increasing fuel usage
     pos = int(np.mean(crabs))
     print(f"{pos = }\tTotal cost: {total_cost(crabs, pos, part_2=True)}")
 
 
 def total_cost(arr: np.ndarray, pos: int, part_2=False) -> int:
+    """Calculates fuel consumption to move crabs at positions in `arr` to `pos`.
+    
+    Part 1: constant fual consumption is absolute value of difference of 
+        current crab position and final position
+    Part 2: each move the crab makes increases fuel consumption by 1.
+        2 moves -> (1 + 2) fuel consumed
+        3 moves -> (1 + 2 + 3) fuel consumed
+        n moves -> (1 + 2 + ... + n) fuel consumed
+        sum(1 to n) = n * (n + 1) / 2
+    """
     cost = np.abs(arr - pos)
     return np.sum(cost * (cost + 1) // 2) if part_2 else np.sum(cost)
 
