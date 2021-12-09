aeb9340dc2cd6e6688c89baeadd2e48fe887d944
addisonhernandez <addison.hernandez@gmail.com>
2021-12-09 14:09:11 -0500

    Refactor and reformat for clarity and docstrings

--- day 4/day4.py
+++ day 4/day4.py
@@ -6,11 +6,11 @@ import numpy as np
 
 
 def main():
-    with open("day4.txt", "r") as infile:
-        # Generate input list
+    with open("day 4/day4.txt") as infile:
+        # Generate input list from first line of infile
         host_calls = np.fromstring(infile.readline(), dtype=int, sep=",")
 
-        # Generate Bingo boards
+        # Generate 5x5 Bingo boards from the rest of the file
         boards = np.fromfile(infile, dtype=int, sep=" ").reshape((-1, 5, 5))
 
     number_of_boards = boards.shape[0]
@@ -21,14 +21,14 @@ def main():
 
     # While no one has won, iterate over input list
     for call in np.nditer(host_calls):
+        # Check against each number on each board
         it = np.nditer(boards, flags=["multi_index"])
-        for board_number in it:
-            if board_number != call:
+        for number in it:
+            if number != call:
                 continue
             # mark boards
             board, row, col = it.multi_index
             marked[board, row, col] = True
-
             # check for a winner
             if not winning_board(marked, board, row, col):
                 continue
@@ -50,6 +50,11 @@ def winning_board(arr: np.ndarray, board: int, row: int, col: int) -> bool:
 
 
 def winning_score(board: np.ndarray, marked: np.ndarray, last_called: int) -> int:
+    """Calculate the score for a winning board.
+
+    Score is calculated as the sum of unmarked numbers multiplied by the last
+    number called.
+    """
     return board.sum(where=np.invert(marked)) * last_called
 
 
